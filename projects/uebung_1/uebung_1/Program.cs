88cfe76f43e5441b389f346583fcee3b64ef7dff
Nino Ninux <nino.ninux@gmail.com>
2016-09-20 21:49:55 +0200

    coded first exercise

---
+++ projects/uebung_1/uebung_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_1
+{
+    class Program
+    {
+        #region methods
+        /// <summary>
+        /// Print message to console
+        /// </summary>
+        /// <param name="msg">Message to be printed</param>
+        /// <param name="upperCase">Print in upper or lower case letters</param>
+        private static void PrintMessage(String msg, bool upperCase)
+        {
+            if (upperCase)
+            {
+                msg = msg.ToUpper();
+            }
+            else
+            {
+                msg = msg.ToLower();
+            }
+
+            Console.WriteLine(msg);
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            PrintMessage("Huhu", true);
+            PrintMessage("Huhu", false);
+            Console.Read();
+        }
+    }
+}
