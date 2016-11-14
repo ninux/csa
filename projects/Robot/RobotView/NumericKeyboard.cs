620fe3766558c0c1fb2f2b5e42430283148c9f5a
Nino Ninux <nino.ninux@gmail.com>
2016-11-14 21:59:45 +0100

    began to implement accumulator logic

--- projects/Robot/RobotView/NumericKeyboard.cs
+++ projects/Robot/RobotView/NumericKeyboard.cs
@@ -10,9 +10,35 @@ namespace RobotView
 {
     public partial class NumericKeyboard : UserControl
     {
+        #region members
+        private int accumulator;
+        private int input_number;
+        private String input_string;
+        #endregion
+
+        #region constructor & destructor
         public NumericKeyboard()
         {
             InitializeComponent();
+            input_number = 0;
+            accumulator = input_number;
+            input_string = "0";
         }
+
+        public NumericKeyboard(int initial)
+        {
+            InitializeComponent();
+            input_number = initial;
+            input_string = input_number.ToString();
+            tb_nk_number.Text = input_string;
+        }
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region methods
+        
+        #endregion methods
     }
 }
